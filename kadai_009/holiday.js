74dfb0758c4254fc6bfe1fcd8310cdd9d37e88cf
taketaka-0223 <156500739+taketaka-0223@users.noreply.github.com>
2024-01-28 14:31:35 +0900

    kadai_009の修正

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -19,21 +19,7 @@ while (count < 1) {
   count++;
 }
 
-for (let i = 0; i < 1; i++) {
-  console.log('正月');
-  console.log('成人の日');
-  console.log('建国記念日');
-  console.log('天皇誕生日');
-  console.log('春分の日');
-  console.log('昭和の日');
-  console.log('憲法記念日');
-  console.log('みどりの日');
-  console.log('こどもの日');
-  console.log('海の日');
-  console.log('山の日');
-  console.log('敬老の日');
-  console.log('秋分の日');
-  console.log('スポーツの日');
-  console.log('文化の日');
-  console.log('勤労感謝の日');
+const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
+for (let item of holidays) {
+  console.log(item);
 }
