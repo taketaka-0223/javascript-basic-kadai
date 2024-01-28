f151580279b205b3dbae2d3d40be818289e8aacf
taketaka-0223 <156500739+taketaka-0223@users.noreply.github.com>
2024-01-28 13:29:51 +0900

    kadai_009の追加

---
+++ kadai_009/holiday.js
@@ -0,0 +1,39 @@
+let count = 0;
+while (count < 1) {
+  console.log('正月');
+  console.log('成人の日');
+  console.log('建国記念日');
+  console.log('天皇誕生日');
+  console.log('春分の日');
+  console.log('昭和の日');
+  console.log('憲法記念日');
+  console.log('みどりの日');
+  console.log('こどもの日');
+  console.log('海の日');
+  console.log('山の日');
+  console.log('敬老の日');
+  console.log('秋分の日');
+  console.log('スポーツの日');
+  console.log('文化の日');
+  console.log('勤労感謝の日');
+  count++;
+}
+
+for (let i = 0; i < 1; i++) {
+  console.log('正月');
+  console.log('成人の日');
+  console.log('建国記念日');
+  console.log('天皇誕生日');
+  console.log('春分の日');
+  console.log('昭和の日');
+  console.log('憲法記念日');
+  console.log('みどりの日');
+  console.log('こどもの日');
+  console.log('海の日');
+  console.log('山の日');
+  console.log('敬老の日');
+  console.log('秋分の日');
+  console.log('スポーツの日');
+  console.log('文化の日');
+  console.log('勤労感謝の日');
+}
